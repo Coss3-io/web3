c857d99f055ace1d9e08b1935b2318629176a1d3
sylar123abc <sylar123abc@gmail.com>
2023-05-04 17:42:52 +0200

    Updating font color

--- src/component/TestComponent.vue
+++ src/component/TestComponent.vue
@@ -3,20 +3,20 @@
     class="p-10 flex justify-between w-10/12 shadow-lg shadow-black m-auto rounded-lg mt-5"
   >
     <div
-      class="h-16 w-44 bg-gray-300 shadow-lg shadow-gray-800 rounded-lg flex items-center justify-center"
+      class="h-16 text-black w-44 bg-gray-300 shadow-lg shadow-gray-800 rounded-lg flex items-center justify-center"
       data-testid="counter"
     >
       Counter value : {{ testStore.test }}
     </div>
     <div
-      class="h-16 w-44 bg-gray-300 shadow-lg shadow-gray-800 rounded-lg flex items-center justify-center"
+      class="h-16 text-black w-44 bg-gray-300 shadow-lg shadow-gray-800 rounded-lg flex items-center justify-center"
       data-testid="getter"
     >
       Getter value : {{ testStore[TestGetters.Test] }}
     </div>
     <div
         @click="testStore[TestActions.Increment]"
-      class="cursor-pointer h-16 w-24 bg-gray-600 shadow-lg shadow-black rounded-lg flex items-center justify-center active:shadow-none hover:shadow-black/50 transition-all"
+      class="cursor-pointer text-black h-16 w-24 bg-gray-600 shadow-lg shadow-black rounded-lg flex items-center justify-center active:shadow-none hover:shadow-black/50 transition-all"
       data-testid="increment"
 
     >
