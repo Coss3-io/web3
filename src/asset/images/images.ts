106d53f2dd569d2b6a66648e9d6f17dd985816cd
sylar123abc <sylar123abc@gmail.com>
2023-11-06 10:58:03 +0100

    Updating trade logo

--- src/asset/images/images.ts
+++ src/asset/images/images.ts
@@ -17,6 +17,7 @@ export { default as amount } from "./amount.vue";
 export { default as price } from "./price.vue";
 export { default as buyLogo } from "./buyLogo.vue";
 export { default as botLogo } from "./botLogo.vue";
+export { default as tradeLogo } from "./tradeLogo.vue";
 export { default as sellLogo } from "./sellLogo.vue";
 export { default as filledOrderLogo } from "./filledOrderLogo.vue";
 export { default as cancelledOrderLogo } from "./cancelledOrderLogo.vue";
--- src/component/pages/Trade.vue
+++ src/component/pages/Trade.vue
@@ -6,20 +6,7 @@
       <div class="col-span-full m-2 flex justify-start">
         <div class="tooltip" data-tip="click for help">
           <button class="flex gap-4 btn btn-ghost text-4xl font-bold">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-12 h-12"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
-              />
-            </svg>
+            <tradeLogo class="w-12 h-12"></tradeLogo>
             Trade
           </button>
         </div>
@@ -81,6 +68,7 @@ import UserOrders from "../sections/Trade/UserOrders.vue";
 import TradeHistory from "../sections/Trade/TradeHistory.vue";
 import NewOrder from "../sections/Trade/NewOrder.vue";
 import BalancesDetails from "../sections/Trade/BalancesDetails.vue";
+import { tradeLogo } from "../../asset/images/images";
 
 const orderDetails = reactive({ price: 0, amount: 0 });
 const tradeHistory = reactive([{ price: 0, amount: 0 }]);
