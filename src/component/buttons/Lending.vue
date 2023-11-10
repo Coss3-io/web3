7e44c560edf9f5615d4f3831c8853242be3ac599
sylar123abc <sylar123abc@gmail.com>
2023-11-10 10:25:00 +0100

    Lending button update

--- src/component/buttons/Lending.vue
+++ src/component/buttons/Lending.vue
@@ -1,11 +1,16 @@
 <template>
-  <a class="btn btn-ghost" @click="unCheck">
-    <lendingLogo class="w-6 h-6"></lendingLogo>
+  <RouterLink
+    :to="{ name: RouteNames.Lending }"
+    class="btn btn-ghost"
+    @click="unCheck"
+  >
+    <fsaLogo class="w-6 h-6"></fsaLogo>
     Lending
-  </a>
+  </RouterLink>
 </template>
 <script setup lang="ts">
 import { lendingLogo } from "../../asset/images/images";
+import { RouteNames } from "../../router";
 
 const unCheck = () => {
   let label = document.getElementById("main-drawer")!;
