7bb6b0c95566b949047c82e4371efd9a62eee850
sylar123abc <sylar123abc@gmail.com>
2023-11-24 11:04:25 +0100

    Adding hpe page log in transitions

--- src/component/pages/Home.vue
+++ src/component/pages/Home.vue
@@ -28,53 +28,58 @@
           enjoy brand new web3 features
         </p>
         <div class="flex gap-5 lg:justify-start justify-center py-2">
-          <RouterLink
-            v-if="accountStore.$state.appConnected"
-            :to="{ name: RouteNames.NewBot }"
-            class="btn btn-primary"
+          <div
+            class="relative flex justify-start items-center transition-all delay-300"
+            :class="accountStore.$state.appConnected ? 'w-32' : 'w-40'"
           >
-            Start now
-          </RouterLink>
-          <button
-            v-else-if="accountStore.$state.blockchainConnected"
-            @click="
-              Client.login();
-            "
-            class="btn btn-primary"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33"
-              />
-            </svg>
-            login (2/2)
-          </button>
-          <button v-else class="btn btn-primary" @click="open()">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M2.25 15a4.5 4.5 0 004.5 4.5H18a3.75 3.75 0 001.332-7.257 3 3 0 00-3.758-3.848 5.25 5.25 0 00-10.233 2.33A4.502 4.502 0 002.25 15z"
-              />
-            </svg>
-            login (1/2)
-          </button>
+            <transition name="fadeNav">
+              <RouterLink
+                v-if="accountStore.$state.appConnected"
+                :to="{ name: RouteNames.NewBot }"
+                class="btn btn-primary"
+              >
+                Start now
+              </RouterLink>
+              <button
+                v-else-if="accountStore.$state.blockchainConnected"
+                @click="Client.login()"
+                class="btn btn-primary"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33"
+                  />
+                </svg>
+                login (2/2)
+              </button>
+              <button v-else class="btn btn-primary" @click="open()">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.25 15a4.5 4.5 0 004.5 4.5H18a3.75 3.75 0 001.332-7.257 3 3 0 00-3.758-3.848 5.25 5.25 0 00-10.233 2.33A4.502 4.502 0 002.25 15z"
+                  />
+                </svg>
+                login (1/2)
+              </button>
+            </transition>
+          </div>
           <RouterLink :to="{ name: RouteNames.NewBot }" class="btn btn-ghost">
             Learn more
           </RouterLink>
