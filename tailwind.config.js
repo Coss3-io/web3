af0ce7e2ffa5638807ebecb5b9df1ad78551ca99
sylar123abc <sylar123abc@gmail.com>
2023-11-27 10:51:05 +0100

    updating primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./src/**/*.vue", "./src/types/*.ts","./public/index.html"],
+  content: ["./src/**/*.vue", "./src/types/*.ts", "./public/index.html"],
   theme: {
     extend: {
       keyframes: {
         slideIn: {
-          "to": { opacity: "1", transform: "translateY(0)" },
+          to: { opacity: "1", transform: "translateY(0)" },
         },
       },
     },
     animation: {
-      slideIn: 'slideIn 0.3s ease-in-out 0.3s forwards',
-      ...defaultTheme.animation
+      slideIn: "slideIn 0.3s ease-in-out 0.3s forwards",
+      ...defaultTheme.animation,
     },
     screens: {
       xs: "385px",
@@ -20,7 +20,14 @@ module.exports = {
     },
   },
   daisyui: {
-    themes: ["dark"],
+    themes: [
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+          primary: "#7e21cf", 
+        },
+      },
+    ],
   },
   variants: {},
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
